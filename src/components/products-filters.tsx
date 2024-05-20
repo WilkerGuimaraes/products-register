c6638959b3db7c2c93405cd40218a54c1821e3ae
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-05-20 16:37:26 -0300

    feat: add Error message to form filter

--- src/components/products-filters.tsx
+++ src/components/products-filters.tsx
@@ -8,8 +8,8 @@ import { Input } from "./ui/input";
 import { z } from "zod";
 
 const productsFiltersSchema = z.object({
-  id: z.string(),
-  name: z.string(),
+  id: z.string().min(1, { message: "Informe o id do produto." }),
+  name: z.string().min(1, { message: "Informe o nome do produto." }),
 });
 
 type ProductsFiltersSchema = z.infer<typeof productsFiltersSchema>;
@@ -17,7 +17,7 @@ type ProductsFiltersSchema = z.infer<typeof productsFiltersSchema>;
 export function ProductsFilters() {
   const [_, setSearchParams] = useSearchParams();
 
-  const { register, handleSubmit } = useForm<ProductsFiltersSchema>({
+  const { register, handleSubmit, formState } = useForm<ProductsFiltersSchema>({
     resolver: zodResolver(productsFiltersSchema),
   });
 
@@ -48,8 +48,23 @@ export function ProductsFilters() {
       onSubmit={handleSubmit(handleFilterProducts)}
       className="flex items-center gap-2"
     >
-      <Input {...register("id")} placeholder="ID do produto" />
-      <Input {...register("name")} placeholder="Nome do produto" />
+      <div className="flex flex-col">
+        <Input {...register("id")} placeholder="ID do produto" />
+        {formState.errors.id && (
+          <span className="text-sm text-red-500 mt-1">
+            {formState.errors.id.message}
+          </span>
+        )}
+      </div>
+
+      <div className="flex flex-col">
+        <Input {...register("name")} placeholder="Nome do produto" />
+        {formState.errors.name && (
+          <span className="text-sm text-red-500 mt-1">
+            {formState.errors.name.message}
+          </span>
+        )}
+      </div>
 
       <Button type="submit" variant="link">
         <Search className="size-4 mr-2" />
