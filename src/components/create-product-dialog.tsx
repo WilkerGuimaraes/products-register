249daf8aa9c2a6cdae11135322a4aa7f41cf3fdb
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-08-22 21:19:46 -0300

    fix URL to create new product

--- src/components/create-product-dialog.tsx
+++ src/components/create-product-dialog.tsx
@@ -34,7 +34,7 @@ export function CreateProductDialog() {
 
   const { mutateAsync: createProductFn } = useMutation({
     mutationFn: async ({ name, price }: CreateProductShema) => {
-      await axios.post(`${import.meta.env.VITE_APP_API_URL}/projects`, {
+      await axios.post(`${import.meta.env.VITE_APP_API_URL}/products`, {
         name,
         price,
       });
